0c4cdb4ec76d820b06519000c727debffb8c6db9
SugitaMasataka <mstk.ambit@gmail.com>
2021-03-07 19:12:30 +0900

    heroku

--- analyze_to_slot/settings.py
+++ analyze_to_slot/settings.py
@@ -30,7 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = True
 
 
 # ALLOWED_HOSTS = ["analyze-to-slot.herokuapp.com"]
@@ -145,6 +146,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-if not DEBUG:
+# if not DEBUG:
+if DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     django_heroku.settings(locals()) #追加
